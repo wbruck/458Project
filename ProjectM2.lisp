9ce58d27ad5bb67178a7742da16293d193b0c877
Wolf <brucknerw@gmail.com>
2016-02-29 20:10:01 -0600

    notes

--- ProjectM2.lisp
+++ ProjectM2.lisp
@@ -1,6 +1,6 @@
 ;M2
 
-start on position one
+start with position-array one
 DFS 
 if salary goes over limit (55000) prune
 if score is less than 'best path' prune
@@ -10,6 +10,6 @@ does the state-visited every need to be checked?
 no
 
 
-successor functions will loop throug list of arrays, of possitions
+successor functions will loop throug list of arrays, of positions
 
 set-difference will return the intersection of 2 lists 
