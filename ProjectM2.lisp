33997cdd41d4ecc7db9c9f9b4c63a82aafa4cc2b
Wolf <brucknerw@gmail.com>
2016-02-29 18:27:35 -0600

    Module2

---
+++ ProjectM2.lisp
@@ -0,0 +1,13 @@
+;M2
+
+start on position one
+DFS 
+if salary goes over limit (55000) prune
+if score is less than 'best path' prune
+- not as easy because score can improve
+
+does the state-visited every need to be checked?
+no
+
+
+successor functions will loop throug list of arrays, of possitions
