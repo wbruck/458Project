d0b53a3b58d750c2d03b63fd2937dc317b121817
Wolf <brucknerw@gmail.com>
2016-03-05 00:00:08 -0500

    module2 psudo code

--- ProjectM2.lisp
+++ ProjectM2.lisp
@@ -1,5 +1,13 @@
 ;M2
 
+;arrays for all the players for each position
+; sort the lists so the the highest values are first
+;  this will facilitate pruning
+; *center* 
+; once a player is selected as the 1st for a group remove from "master"
+
+
+
 start on position one
 DFS 
 if salary goes over limit (55000) prune
@@ -10,6 +18,45 @@ does the state-visited every need to be checked?
 no
 
 
-successor functions will loop throug list of arrays, of possitions
+
+;
+;;
+;;;
+;;;;successor functions will loop throug list of arrays, of possitions
+;;;
+;;
+;
+
+; GOAL STATE - the highest points total of all players, yet found
+;recursively loop through arrays of players
+; ?? add to a queue??? try to recurse...
+; 
+; returning the next player on the list
+; for each return check the salary, if over 55k break
+; for each return check "goal state" 
+
+(defun recursive-DFS-player-scorer (position array index, player array index)
+  (cond 
+   ((if salary > 55000) break )
+   ((if number_players_selected < 9) 
+    RECURSIVE-CALL((1+ position_array_index) player_array_index))))
+   
+(defun recursive-dfs (array-arg)
+  (loop
+    while player in list
+    (let player (pop list)
+      (if (eq list last)
+          (loop
+            while
+            pop.list)
+        (recursive-dfs (1+ list))))))
+    
+  pop player from array
+  ;do some calculation
+  (cond 
+  recursive-dfs (next-array)
+     
+     ;
+   
 
 set-difference will return the intersection of 2 lists 
